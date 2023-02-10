40901b142d52c0408a569608f7c483ff177a7c08
DannyZahariev <zaharied@oregonstate.edu>
2023-02-09 19:40:54 -0800

    intial commit

--- index.py
+++ index.py
@@ -1,69 +1,17 @@
 import boto3
-# presiged url trigger
-import json
 import logging
 from botocore.exceptions import ClientError
 
-
-def create_presigned_url(bucket_name, object_name, expiration=3600):
-    """Generate a presigned URL to share an S3 object
-
-    :param bucket_name: string
-    :param object_name: string
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Presigned URL as string. If error, returns None.
-    """
-
-    # Generate a presigned URL for the S3 object
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.generate_presigned_url('get_object',
-                                                    Params={'Bucket': bucket_name,
-                                                            'Key': object_name},
-                                                    ExpiresIn=expiration)
-    except ClientError as e:
-        logging.error(e)
-        return None
-
-    # The response contains the presigned URL
-    return response
-
-
-# upload_test_jeff2
-# import boto3
-# import os
-# import json
-
 BUCKET_NAME = "osuapp-winter2023"
-
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     # parse json in event to get key
-    key = json.loads(event)['Records'][0]['s3']['object']['key']
-
-    # Create a connection to S3
-    s3 = boto3.client('s3')
-    # Set the bucket name where the file is stored
-    # bucket_name = BUCKET_NAME
-    # List all objects in the bucket
-    # result = s3.list_objects_v2(Bucket=bucket_name)
-    # Store the list of objects in a variable
-    # objects = result.get('Contents', [])
-    # sorted_objects = sorted(
-    #     objects, key=lambda x: x['LastModified'], reverse=True)     # Sort the objects by date created
-    # Create an array to store the names of the files in the bucket
-    # filearray = [key]
-    # for obj in sorted_objects:                          # For each object in the bucket
-    #     # Print the key (file name)
-    #     print(obj['Key'])
-    #     # Add the key to the filearray variable
-    #     filearray.append(obj['Key'])
-    # Print the file name of the most recently created file
+    key = event['Records'][0]['s3']['object']['key']
 
     print('filename: ' + key)
 
     # Set the source bucket name
-    src_bucket_path = BUCKET_NAME
     # Set the source file name
     src_file_name = key
     if ((src_file_name.find('/')) != (-1)):             # If the filename contains a forward slash
@@ -75,7 +23,6 @@ def lambda_handler(event, context):
         # Set the target filename
         tgt_file_name = 'gltb/' + src_file_name # temporarily use the same input file and send it back
         pass
-    tgt_bucket_path = BUCKET_NAME               # Set the target bucket name
 
     # copy a file from one S3 bucket to another S3 bucket
     # src_bucket_path = 's3://sourcebucket'
@@ -87,44 +34,35 @@ def lambda_handler(event, context):
     print('src' + src_file_name)
     print('tgt' + tgt_file_name)
 
-    # get the source file
-    s3 = boto3.resource('s3')
-    copy_source = {
-        'Bucket': src_bucket_path,
-        'Key': src_file_name
-    }
+    # TODO: download the file from s3 locally
+    response = s3.get_object(Bucket=BUCKET_NAME, Key='metadata.csv')
+    file_content = response['Body'].read().decode('utf-8')
 
-    # copy the source file to the target bucket
-    # s3.meta.client.copy(copy_source, tgt_bucket_path, tgt_file_name)
+    with open('/tmp/file.stl', 'w') as f:
+        f.write(file_content)
+        
+    localFileName = "/tmp/file.stl"
+    upload_to_s3(localFileName, tgt_file_name)
 
-    # download the file
-    file = 
+    # file name with extension
+    localFileName = os.path.basename('/root/file.ext')
+    # file name without extension
+    print(os.path.splitext(file_name)[0])
 
-# file_upload
-# import boto3
-# import json
-# import zipfile
+    create_presigned_url(tgt_file_name)
 
+    # TODO: download metadata.csv and save to lambda temp folder
+    response = s3.get_object(Bucket=BUCKET_NAME, Key='metadata.csv')
+    file_content = response['Body'].read().decode('utf-8')
+    new_file_content = 'new_file_name,new_metadata\n'
 
-BUCKET_NAME = "osuapp-file-upload-bucket"
-s3 = boto3.client('s3')
+    with open('/tmp/metadata.csv', 'w') as f:
+        # append metadata.csv with new file name and metadata
+        f.write(new_file_content)
+    
 
-
-def lambda_handler(event, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-
-    print(bucket)
-    print(key)
-
-    # with zipfile.ZipFile('test3.zip', 'r') as zip_ref:
-    #     zip_ref.extractall('')
-    upload_to_s3("test.txt", "test.txt")
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('file_upload lambda')
-    }
+    # TODO: upload metadata.csv to s3
+    upload_to_s3('/tmp/metadata.csv', 'metadata.csv')
 
 
 # upload local file to s3 bucket
@@ -133,21 +71,34 @@ def lambda_handler(event, context):
 def upload_to_s3(local_file, s3_file):
     try:
         s3.upload_file(local_file, BUCKET_NAME, s3_file)
-        # generates temporary link for 1 day
-        url = s3.generate_presigned_url(
-            ClientMethod='get_object',
-            Params={
-                'Bucket': BUCKET_NAME,
-                'Key': s3_file
-            },
-            ExpiresIn=24 * 3600
-        )
 
         print("Upload Successful", url)
-        return url
-    except FileNotFoundError:
-        print("File not found")
         return None
-    except NoCredentialsError:
-        print("Credentials not available")
+    except FileNotFoundError as e:
+        print("File not found: ", e)
+        return e
+    except NoCredentialsError as e:
+        print("Credentials not available", e)
+        return e
+
+def create_presigned_url(key_object_name, expiration=3600):
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+
+    # Generate a presigned URL for the S3 object
+    try:
+        response = s3.generate_presigned_url('get_object',
+                                                    Params={'Bucket': BUCKET_NAME,
+                                                            'Key': key_object_name},
+                                                    ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
         return None
+
+    # The response contains the presigned URL
+    return response
--- stl_to_gltf.py
+++ stl_to_gltf.py
@@ -1,1 +1,10 @@
-import aspose.threed as a3d
+import glob
+import os
+
+def get_stl_files(current_dir):
+      stl_files = glob.glob(os.path.join(current_dir, "*.stl"))
+      return stl_files
+
+if __name__ == "__main__":
+      current__directory = os.path.dirname(os.path.abspath(__file__))
+      print(get_stl_files(current__directory))
